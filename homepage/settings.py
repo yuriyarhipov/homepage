fdc54fb73c9f1a183e00e0e28707ae6362d479bd
Yuriy Arhipov <yura@pragmatic.software>
2021-12-31 02:44:54 +0300

    tailwind

--- homepage/settings.py
+++ homepage/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "main",
     "blog",
+    "tailwind",
+    "theme",
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
