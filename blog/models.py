856aaff2c1ec082f3d44b37ac9049fdd1aa12eb7
Yuriy Arhipov <yura@pragmatic.software>
2021-12-31 03:26:09 +0300

    fix styles

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
     date = models.DateField()
+
+    def __str__(self):
+        return self.title
