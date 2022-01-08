2fdbad8553dbebf09a1dad1a2753344d290403f2
Yuriy Arhipov <yura@pragmatic.software>
2022-01-08 20:27:16 +0300

    add subscribtio service

--- blog/models.py
+++ blog/models.py
@@ -3,8 +3,15 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    preview = models.TextField()
     body = models.TextField()
     date = models.DateField()
+    mail_list = models.ForeignObject("MailList", on_delete=models.DO_NOTHING, null=True)
 
     def __str__(self):
         return self.title
+
+class MailList(models.Model):
+    date = models.DateField()
+    title = models.TextField()
+    main_text = models.TextField()
