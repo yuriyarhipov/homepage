78dabf2b74e83b7ff3354b99e1e34dba6ddae168
Yuriy Arhipov <yarkhipov@bmat.com>
2022-02-07 00:30:09 +0300

    deploy do

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from blog.models import Post
+from blog.models import Post, MailList
 # Register your models here.
 
 admin.site.register(Post)
+admin.site.register(MailList)
--- blog/models.py
+++ blog/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
-    preview = models.TextField()
+    preview = models.TextField(default="")
     body = models.TextField()
     date = models.DateField()
-    mail_list = models.ForeignObject("MailList", on_delete=models.DO_NOTHING, null=True)
+    mail_list = models.ForeignKey("MailList", on_delete=models.DO_NOTHING, null=True, blank=True)
 
     def __str__(self):
         return self.title
